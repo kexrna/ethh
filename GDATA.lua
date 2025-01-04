fb1ec2d696e46f06eebc9f12323e17b414fc35e3
kexrna <inmatevibhuya@gmail.com>
2025-01-04 14:35:38 +0100

    Create GDATA.lua

---
+++ GDATA.lua
@@ -0,0 +1,16 @@
+local workspace = game:GetService("Workspace")
+return {
+    [2788229376] = {Argument = "UpdateMousePosI2", Remote = "MainEvent", Version = 1537, Bullet_Path = workspace:FindFirstChild("Ignored") and workspace.Ignored:FindFirstChild("Siren") and workspace.Ignored.Siren:FindFirstChild("Radius") or nil, bullet_name = "BULLET_RAYS"},
+    [7213786345] = {Argument = "UpdateMousePosI2", Remote = "MainEvent", Bullet_Path = workspace:FindFirstChild("Ignored") and workspace.Ignored:FindFirstChild("Siren") and workspace.Ignored.Siren:FindFirstChild("Radius") or nil, bullet_name = "BULLET_RAYS"},
+    [16033173781] = {Argument = "UpdateMousePosI2", Remote = "MainEvent", Version = 1537, Bullet_Path = workspace:FindFirstChild("Ignored") and workspace.Ignored:FindFirstChild("Siren") and workspace.Ignored.Siren:FindFirstChild("Radius") or nil, bullet_name = "BULLET_RAYS"},
+    [9825515356] = {Argument = "MousePosUpdate", Remote = "MainEvent", Version = 4411, Bullet_Path = workspace:FindFirstChild("Ignored") or nil, bullet_name = "BULLET_RAYS"},
+    [5602055394] = {Argument = "MousePos", Remote = "Bullets"},
+    [9183932460] = {Argument = "UpdateMousePos", Remote = ".gg/untitledhood"},
+    [14412355918] = {Argument = "MOUSE", Remote = "MAINEVENT"},
+    [14412601883] = {Argument = "MOUSE", Remote = "MAINEVENT"},
+    [14412436145] = {Argument = "MOUSE", Remote = "MAINEVENT"},
+    [14487637618] = {Argument = "MOUSE", Remote = "MAINEVENT"},
+    [11143225577] = {Argument = "UpdateMousePos", Remote = "MainEvent"},
+    [14413712255] = {Argument = "MOUSE", Remote = "MAINEVENT"},
+    [14472848239] = {Argument = "MoonUpdateMousePos", Remote = "MainEvent"},
+}
