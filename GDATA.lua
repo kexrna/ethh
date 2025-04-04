9ee3d2ee40845e524a887973ff1760b2d55c7080
kexrna <inmatevibhuya@gmail.com>
2025-04-04 14:38:26 +0200

    Update GDATA.lua

--- GDATA.lua
+++ GDATA.lua
@@ -1,9 +1,28 @@
 local workspace = game:GetService("Workspace")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
 return {
-    [2788229376] = {Argument = "UpdateMousePosI2", Remote = "MainEvent", Version = 1537, Bullet_Path = workspace:FindFirstChild("Ignored") and workspace.Ignored:FindFirstChild("Siren") and workspace.Ignored.Siren:FindFirstChild("Radius") or nil, bullet_name = "BULLET_RAYS"},
-    [7213786345] = {Argument = "UpdateMousePosI2", Remote = "MainEvent", Bullet_Path = workspace:FindFirstChild("Ignored") and workspace.Ignored:FindFirstChild("Siren") and workspace.Ignored.Siren:FindFirstChild("Radius") or nil, bullet_name = "BULLET_RAYS"},
-    [16033173781] = {Argument = "UpdateMousePosI2", Remote = "MainEvent", Version = 1537, Bullet_Path = workspace:FindFirstChild("Ignored") and workspace.Ignored:FindFirstChild("Siren") and workspace.Ignored.Siren:FindFirstChild("Radius") or nil, bullet_name = "BULLET_RAYS"},
-    [9825515356] = {Argument = "MousePosUpdate", Remote = "MainEvent", Version = 4411, Bullet_Path = workspace:FindFirstChild("Ignored") or nil, bullet_name = "BULLET_RAYS"},
+    [2788229376] = {
+                Argument = "UpdateMousePosI2",
+                Remote = "MainEvent", 
+                Version = 1537, 
+                Bullet_Path = workspace:FindFirstChild("Ignored") and workspace.Ignored:FindFirstChild("Siren") and workspace.Ignored.Siren:FindFirstChild("Radius") or nil, 
+                bullet_name = "BULLET_RAYS",
+                Shop_Folder = workspace:FindFirstChild("Ignored") or nil
+            },
+    [9825515356] = {
+                Argument = "MousePosUpdate", 
+                Remote = "MainEvent", 
+                Version = 4411, 
+                Bullet_Path = workspace:FindFirstChild("Ignored") or nil, 
+                bullet_name = "BULLET_RAYS",
+                Reload = function (Tool, Client)
+                    ReplicatedStorage.MainEvent:FireServer("Reload", Tool)
+                    local Anim = Client.Character.Humanoid:LoadAnimation(ReplicatedStorage.Animations.ReloadAnim)
+					Anim:Play()
+					game.Debris:AddItem(Anim, 2)
+                end,
+                ReloadV = "Reloading_CLIENT"
+            },
     [5602055394] = {Argument = "MousePos", Remote = "Bullets"},
     [9183932460] = {Argument = "UpdateMousePos", Remote = ".gg/untitledhood"},
     [14412355918] = {Argument = "MOUSE", Remote = "MAINEVENT"},
